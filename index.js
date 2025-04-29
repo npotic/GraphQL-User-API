d26897f07adf4cdc838acb61f284605186c0e7f9
npotic <x.nemanjapotic.x@gmail.com>
2025-04-29 20:55:49 +0200

    delete and filter

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 let users = [];
+
 const typeDefs = gql`
   type User {
     id: ID!
@@ -9,18 +10,36 @@ const typeDefs = gql`
   }
 
   type Query {
-    users: [User!]!
+    users(name: String, email: String): [User!]!
   }
 
   type Mutation {
     addUser(name: String!, email: String!): User!
+    deleteUser(id: ID!): User!
   }
 `;
 
 const resolvers = {
     Query: {
-        users: () => users,
+        users: (_, { name, email }) => {
+            let filtered = users;
+
+            if (name) {
+                filtered = filtered.filter(user =>
+                    user.name.toLowerCase().includes(name.toLowerCase())
+                );
+            }
+
+            if (email) {
+                filtered = filtered.filter(user =>
+                    user.email.toLowerCase().includes(email.toLowerCase())
+                );
+            }
+
+            return filtered;
+        },
     },
+
     Mutation: {
         addUser: (_, { name, email }) => {
             if (!email.includes('@')) {
@@ -28,14 +47,25 @@ const resolvers = {
             }
 
             const newUser = {
-                id: users.length + 1, 
+                id: users.length + 1,
                 name,
                 email,
             };
 
             users.push(newUser);
-
             return newUser;
+        },
+
+        deleteUser: (_, { id }) => {
+            const userIndex = users.findIndex(user => user.id === parseInt(id));
+
+            if (userIndex === -1) {
+                throw new Error('Korisnik nije pronađen.');
+            }
+
+            const deleted = users[userIndex];
+            users.splice(userIndex, 1);
+            return deleted;
         },
     },
 };
@@ -45,6 +75,6 @@ const server = new ApolloServer({
     resolvers,
 });
 
-server.listen().then(({ url }) => {
+server.listen({ port: 4000 }).then(({ url }) => {
     console.log(`Server je pokrenut na: ${url}`);
 });
